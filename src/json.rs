34cf634b7189968912eb7b542560b93084a62a7f
Maxime <maxime@echapp.com>
2023-11-02 01:35:31 +0100

    It works

--- src/json.rs
+++ src/json.rs
@@ -1,63 +1,41 @@
+use std::io::Read;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct JsonData(Vec<JsonStructure>);
+pub struct JsonData(pub Vec<JsonStructure>);
 
-
-/// Example :
-/// 
-/// ```json
-///{
-/// "id": 27891,
-/// "name": "ribbit",
-/// "organization_name": "ribbit",
-/// "platform": "GitLab",
-/// "repository_url": "https://gitlab.inria.fr/ribbit/ribbit",
-/// "description": "🐸 https://ribbit.gitlabpages.inria.fr/ribbit/ 🐸",
-/// "default_branch": "master",
-/// "is_fork": null,
-/// "is_archived": false,
-/// "creation_date": "2021-02-21T16:52:46Z",
-/// "last_update": "2023-10-26T13:54:39Z",
-/// "last_modification": "2023-10-26T13:54:39Z",
-/// "homepage": null,
-/// "stars_count": 0,
-/// "forks_count": 1,
-/// "license": null,
-/// "open_issues_count": 0,
-/// "language": null,
-/// "topics": "",
-/// "software_heritage_exists": false,
-/// "software_heritage_url": null
-///}
-///```
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct JsonStructure {
-  pub id: u32,
-  pub name: String,
-  pub organization_name: String,
-  pub platform: String,
-  pub repository_url: String,
-  pub description: String,
-  pub default_branch: String,
-  pub is_fork: bool,
-  pub is_archived: bool,
-  pub creation_date: String,
-  pub last_update: String,
-  pub last_modification: String,
-  pub homepage: String,
-  pub stars_count: u32,
-  pub forks_count: u32,
-  pub license: String,
-  pub open_issues_count: u32,
-  pub language: String,
-  pub topics: String,
-  pub software_heritage_exists: bool,
-  pub software_heritage_url: String,
+    pub id: u32,
+    pub name: Option<String>,
+    pub organization_name: Option<String>,
+    pub platform: Option<String>,
+    pub repository_url: Option<String>,
+    pub description: Option<String>,
+    pub default_branch: Option<String>,
+    pub is_fork: Option<bool>,
+    pub is_archived: Option<bool>,
+    pub creation_date: Option<String>,
+    pub last_update: Option<String>,
+    pub last_modification: Option<String>,
+    pub homepage: Option<String>,
+    pub stars_count: u32,
+    pub forks_count: u32,
+    pub license: Option<String>,
+    pub open_issues_count: u32,
+    pub language: Option<String>,
+    pub topics: Option<String>,
+    pub software_heritage_exists: Option<bool>,
+    pub software_heritage_url: Option<String>,
 }
 
+pub fn read_from_file(path: &str) -> JsonData {
+    let mut file = std::fs::File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    serde_json::from_str(&contents).unwrap()
+}
 
 #[cfg(test)]
-mod tests {
-
-}
+mod tests {}
